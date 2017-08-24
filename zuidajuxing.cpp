bae81d411b80a1f93430c9c31cc24b81a28660ac
QChiLan <1508762731@qq.com>
2017-08-24 09:30:22 +0800

    添加新文件 zuidajuxing.cpp

---
+++ zuidajuxing.cpp
@@ -0,0 +1,35 @@
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cstring>
+using namespace std;
+const int N = 1003;
+int a[N];
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        for(int i=0; i<n; ++i)
+        {
+            cin>>a[i];
+        }
+        int ans=-1;
+        for(int i= 0; i<n; ++i)
+        {
+            int low=a[i];
+            for(int j=i; j< n; ++j)
+            {
+                if(low>a[j])
+                    low=a[j];
+                int temp=(j-i+1)*low;
+                if(temp>ans)
+                    ans=temp;
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
